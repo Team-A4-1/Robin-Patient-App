0a234bcd249bfb7c5da1e2e7b697f417aa62eed6
florianmac <nairolf3>
2021-03-08 17:50:43 +0100

    multiple faces now apear

--- app/src/main/java/com/app/robinasistant/ChooseUser.java
+++ app/src/main/java/com/app/robinasistant/ChooseUser.java
@@ -1,14 +1,17 @@
 package com.app.robinasistant;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -22,55 +25,95 @@ public class ChooseUser extends AppCompatActivity {
 
         TableLayout clients = (TableLayout) findViewById(R.id.users);
 
-        //create tabel row
-        TableRow row = new TableRow(this);
+        int amount = 6;
+        LinearLayout linear =null;
+        TableRow row =null;
 
-        //create constraint layout and constrain set
-        ConstraintLayout cons = new ConstraintLayout(this);
-        ConstraintSet constraintSet = new ConstraintSet();
-
-
+for(int i=0;i<amount;i++){
         //create card view
         CardView corners = new CardView(this);
+        corners.setId(View.generateViewId());
         corners.setLayoutParams(new FrameLayout.LayoutParams(
-                CardView.LayoutParams.WRAP_CONTENT,
-                CardView.LayoutParams.WRAP_CONTENT
-                ));
+                350,
+                350
+        ));
         corners.setRadius(360);
+
+
+        //create text
+        TextView text = new TextView(this);
+        text.setId(View.generateViewId());
+        text.setTextSize(30);
+        text.setText("name");
 
 
         //create imageview and set image
         ImageView picture = new ImageView(this);
+        picture.setId(View.generateViewId());
         picture.setImageDrawable(getResources().getDrawable(R.drawable.mike_andrei_433));
-
-        //create and set text
-        TextView text = new TextView(this);
-        text.setText("name");
-
-
-
-        //set constraints
-        constraintSet.connect(picture.getId(),constraintSet.TOP,text.getId(),constraintSet.BOTTOM,10);
-
-
-
-
-        //addimage to card
         corners.addView(picture);
 
 
-
-        //add corners to constraint layout
+        //create constraint layout
+        ConstraintLayout cons = new ConstraintLayout(this);
+        cons.setId(View.generateViewId());
+        cons.setLayoutParams(new ViewGroup.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        ));
+        //add corneres
         cons.addView(corners);
         cons.addView(text);
 
+//test
 
-        //add picture and text to row
-        row.addView(cons);
+    if (i % 2 == 0) {
+
+        //create linear layout
+        linear = new LinearLayout(this);
 
 
 
+        linear.setId(View.generateViewId());
+        linear.setOrientation(LinearLayout.HORIZONTAL);
+//            linear.setLayoutParams(new LinearLayout.LayoutParams(
+//                   100,
+//                   100
+//
+//                    ));
+
+
+        //create tabel row
+        row = new TableRow(this);
+        row.addView(linear);
         //add  row to table
         clients.addView(row);
+    }
+        linear.addView(cons);
+
+
+
+
+
+
+        // constrain set start
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(cons);
+
+        //constrainset image
+        constraintSet.connect(corners.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
+        constraintSet.connect(corners.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 1);
+        constraintSet.connect(corners.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 1);
+
+        //constrainset text
+        constraintSet.connect(text.getId(), ConstraintSet.TOP, corners.getId(), ConstraintSet.BOTTOM, 10);
+        constraintSet.connect(text.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+        constraintSet.connect(text.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
+//        constraintSet.applyTo(cons);
+
+
+        constraintSet.applyTo(cons);
+    }
+
     }
 }

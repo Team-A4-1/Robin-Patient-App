01cd6ee472307e8bd93c8a58b74a86cda25e34c6
florianmac <nairolf3>
2021-03-16 10:27:28 +0100

    table rows

--- app/src/main/java/com/app/robinasistant/calender.java
+++ app/src/main/java/com/app/robinasistant/calender.java
@@ -2,13 +2,36 @@ package com.app.robinasistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
+import android.widget.TableRow;
+
+import java.util.Calendar;
 
 public class calender extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TableRow row1 =  findViewById(R.id.row1);
+        TableRow row2 =  findViewById(R.id.row2);
+        TableRow row3 =  findViewById(R.id.row3);
+        TableRow row4 =  findViewById(R.id.row4);
+
+        TableRow[] rows = {row1,row2,row3,row4};
+
+        Calendar date = Calendar.getInstance();
+
+        for(int i=0 ;i<rows.length;i++){
+            for(int j=0;j<7;j++){
+
+
+            }
+        }
+
+
         setContentView(R.layout.activity_calender);
+
     }
 }
